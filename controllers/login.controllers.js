4237edab775736cd5251779732039dd59e37fdba
Silvannnnna <152339302+Silvannnnna@users.noreply.github.com>
2026-05-11 19:09:19 -0600

    Hashing Passwords

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,10 +1,12 @@
 import User from '../models/users.model.js';
+import {hash} from '../utils/hash.js'
 
 export const login = async (req,res) =>{
     const {username,password} = req.body
-    const user = await User.findOne
-    ({username:username})
-    if (user.password === password) {
+    const user = await User.findOne({username:username})
+    const salt = user.password.substring(0, process.env.SALT_SIZE)
+    const hashed = hash(password, salt)
+    if (user.password === hashed) {
         res.json({login:true, msg:"Login successful", user:user})
     }else {
         res.status(404).json({login:false, msg:"Login failed", user:{}})
