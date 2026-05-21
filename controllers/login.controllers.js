c941a18036b50358c6a1d0e5234708014dc40d61
Silvannnnna <152339302+Silvannnnna@users.noreply.github.com>
2026-05-21 11:46:52 -0600

    correccion

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -5,14 +5,16 @@ import jwt from 'jsonwebtoken'
 export const login = async (req,res) =>{
     const {username,password} = req.body
     const user = await User.findOne({username:username})
-    const salt = user.password.substring(0, process.env.SALT_SIZE)
+    if (!user) {
+        return res.status(401).json({login:false, msg:"Usuario o contraseña incorrectos", user:{}, token:""})
+    }
+    const salt = user.password.substring(0, Number(process.env.SALT_SIZE))
     const hashed = hash(password, salt)
     if (user.password === hashed) {
-        const token = jwt.sign({sub:user._id}, process.env.JWT, 
-        {expiresIn:"1h"})
-        res.json({login:true, msg:"Ok", user:user, token:token})
-    }else {
-        res.status(404).json({login:false, msg:"wrong", user:{}, token:""})
+        const token = jwt.sign({sub:user._id}, process.env.JWT, {expiresIn:"1h"})
+        res.json({login:true, msg:"Ok", user:{_id:user._id, name:user.name, username:user.username}, token:token})
+    } else {
+        res.status(401).json({login:false, msg:"Usuario o contraseña incorrectos", user:{}, token:""})
     }
 }
 
