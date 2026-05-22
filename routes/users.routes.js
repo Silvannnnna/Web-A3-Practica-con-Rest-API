ddc1ef0ae98adfb0c5d6749a3b5b928e2f651b48
Silvannnnna <152339302+Silvannnnna@users.noreply.github.com>
2026-05-22 07:56:17 -0600

    correcion de users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -7,7 +7,7 @@ const router = Router()
 
 router.get("/users", validateJWT, getUsers)
 router.get("/users/:id", validateJWT, getUser)
-router.post("/users", validateJWT, postUser)
+router.post("/users", postUser)
 router.put("/users/:id", validateJWT, putUser)
 router.delete("/users/:id", validateJWT, delUser)
 
